c3faa1bfb22dd07c6004da39850cfc4c88828004
Ashish <phularaashish12345@gmail.com>
2023-03-03 23:14:52 +0530

    added files for ui of login and register

--- eventproposal/src/Components/Home.jsx
+++ eventproposal/src/Components/Home.jsx
@@ -1,33 +1,77 @@
-import React from 'react';
-import "../Components/css/home.css";
-import img1 from "../assets/party.png";
-import img2 from "../assets/symbol.png";
-import {Link} from "react-router-dom"
+import React from 'react'
+import { useState } from 'react';
+import { Link } from  "react-router-dom";
+// form page for vendor to create proposals
+import "../Components/css/home.css"
 
-const Home = () => {
+const CreateProposals = () => {
+const [eventName, seteventName] = useState("");
+const [eventplace, seteventPlace] = useState('');
+const [proposalType, setproposalType] = useState('');
+const [eventType, seteventType] = useState('')
+const [eventClass, seteventClass] = useState("");
+const [budget, setbudget] = useState('');
+const [fromDate, setfromDate] = useState('');
+const [toDate, settoDate] = useState('');
+const [description, setdescription] = useState('');
+const [foodPreferences, setfoodPreferences] = useState('');
+const [events, setevents] = useState('');
+const [images, setimages] = useState('');
+
+const uploadEvent = () =>{
+  const formData = new FormData();
+  formData.append('eventName', eventName);
+  formData.append('eventPlace', eventplace);
+  formData.append('proposalType', proposalType);
+  formData.append('eventType', eventType);
+  formData.append('eventClass', eventClass);
+  formData.append('budget', budget);
+  formData.append('fromDate', fromDate);
+  formData.append('toDate', toDate);
+  formData.append('description', description);
+  formData.append('foodPreferences', foodPreferences);
+  formData.append('events', events);
+  formData.append('images', images)
+
+  // for(let i = 0; i < images.length; i++) {
+  //   formData.append('images', images[i]);
+  // }
+
+  formData.forEach((val, key) => {
+    console.log(key, val);
+  })
+  //console.log(formData);
+  fetch('http://localhost:5000/createproposals', {
+    method: 'POST',
+    body: formData,
+  })
+}
+
   return (
-    <div className='home'>
-      <img src={img1} alt='party' className='party'/>
-      <img src={img2} alt='symbol' className='symbol'/>
-      <div className='formContainer'>
-        <button className='vendor-btn'>Vendor</button>
-        <button className='user-btn'>User</button>
-        <div className='signup-text'>Sig in Your Account </div>
-        <div className='input-boxes'>
-          <input className='phone-input' placeholder='Phone...'/>
-      <input className='password-input' placeholder='Password...'/>
-        </div>
-      
-      <p className='forgot-text'>Forgot Password.?</p>
-      <Link className="link-buttons"> 
-      <p className='create-user'>Create Account</p>
-      <span ><button className='sign-in'>Sign In</button></span>
-      </Link>
-     
-      </div>
-     
+    <div id="containers-prop">
+   <div id='main-box'>
+    <button id='add-button-1'>Add</button>
+      <input id='event_name'  value={eventName} onChange={(e)=> seteventName(e.target.value)} placeholder='eventName'/>
+      <input id='event_place' value={eventplace} onChange={(e)=> seteventPlace(e.target.value)} placeholder='eventPlace'/>
+      <input  id='proposal_type' value={proposalType} onChange={(e)=> setproposalType(e.target.value)} placeholder='proposalType'/>
+      <input  id='event_type' value={eventType} onChange={(e)=> seteventType(e.target.value)} placeholder='eventType'/>
+      <input  id='event_class' value={eventClass} onChange={(e)=> seteventClass(e.target.value)} placeholder='eventClass'/>
+      <input id='budget' type="number"  value={budget} onChange={(e)=> setbudget(e.target.value)} placeholder=' budget '/>
+      <input  id='from_date' type="date" value={fromDate} onChange={(e)=> setfromDate(e.target.value)} placeholder='fromDate'/>
+      <input  id='to_date' type="date" value={toDate} onChange={(e)=> settoDate(e.target.value)} placeholder='toDate'/>
+      <textarea id='description' value={description} onChange={(e)=> setdescription(e.target.value)} placeholder='description'></textarea>
+      <textarea id='food_preferences' value={foodPreferences} onChange={(e)=> setfoodPreferences(e.target.value)} placeholder='foodPreferences'> </textarea>
+      <textarea id='events' value={events} onChange={(e)=> setevents(e.target.value)} placeholder=' events'></textarea>
+      <input id='images' type="file"  onChange={(e)=> setimages(e.target.files[0])} multiple/>
+<Link to="/allproposals">
+      <button id='add_button' onClick={uploadEvent}> ADD </button></Link>
     </div>
+    </div>
+
   )
 }
 
-export default Home;
+export default CreateProposals
+
+
+//eventName,  eventPlace, proposalType, eventType, eventClass, budget, fromDate, toDate, description, foodPreferences, events,images
