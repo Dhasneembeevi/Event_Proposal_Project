3bed9a1e197ddd8647620abeed19fa9474268604
Dhasneembeevi <dhasmohamed1020@gmail.com>
2023-03-04 01:09:56 +0530

    added vendor's css with jsx and for every other component, fixed some bugs

--- eventproposal/src/Components/Proposals/Selected.jsx
+++ eventproposal/src/Components/Proposals/Selected.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
  
+import img3 from "../../Assets/bg party.jpg";
 // proposals selected by user.. with trash icon 
 
 function Selected() {
   return (
     <div>
-      
+    <Header/>
+    <img src={img3} id="image" alt="user" />
     </div>
   )
 }
